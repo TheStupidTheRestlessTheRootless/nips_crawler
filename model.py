267715356b9d2bbf4aad4096ed7a0f669274b8b4
ziming.zhang <zmzhang@Frank.local>
2018-10-26 23:05:37 +0800

    add model

---
+++ model.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+class Record():
+    def __init__(self, name):
+        self.name = name
+        self.organization = ""
+        self.lab = ""
+        self.first = []
+        self.other = []
+
+    def set_name(self, name):
+        self.name = name
+
+    def add_first(self, article):
+        self.first.append(article)
+    
+    def rm_first(self, article):
+        self.first.remove(article)
+
+    def add_other(self, article):
+        self.other.append(article)
+    
+    def rm_other(self, article):
+        self.other.remove(article)
