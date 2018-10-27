f7348c82ab4c01d3cbfeb89f011dabf8874a4894
byunicorn <by.mariana.trench@gmail.com>
2018-10-27 21:18:52 +0800

    feat: add lab info

--- lab/index.js
+++ lab/index.js
@@ -1,8 +1,11 @@
 const puppeteer = require("puppeteer");
 const _ = require("lodash");
-const mockData = require('../mock.json');
+const fs = require("fs");
+// const data = require("../mock.json");
+const data = require("../output.json");
 
 const google_advanced_search = "https://www.google.com/advanced_search";
+
 const key_word = "lab";
 const stop_words = ["the", "at", "in"];
 const punctuation = [".", ",", "!", ":"];
@@ -28,39 +31,49 @@ const parseLab = txt => {
     return lab.join(" ");
 };
 
+const timeout = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+let browser;
+
+const doAdvancedQuery = async record => {
+    const { name, organization: org } = record;
+    const page = await browser.newPage();
+    await page.setViewport({ width: 1200, height: 1500 });
+    await page.goto(google_advanced_search);
+
+    await page.waitForSelector(".otByu > .yIQOUb > .jYcx0e > .PLLUQc > #CwYCWc");
+    await page.type(".otByu > .yIQOUb > .jYcx0e > .PLLUQc > #CwYCWc", `"${name}" "${org}" "lab"`);
+
+    await page.waitForSelector("form > .otByu > .yIQOUb > .jYcx0e > .jfk-button");
+    await page.click("form > .otByu > .yIQOUb > .jYcx0e > .jfk-button");
+
+    await page.waitForNavigation();
+
+    await page.waitForSelector(".g .rc .s", { timeout: 1000 });
+    let res = await page.$(".g .rc .s");
+    const val = await page.evaluate(el => el.innerText, res);
+    const lab = parseLab(val);
+    record.lab = lab;
+    fs.writeFileSync(`./temp/${record.id}`, JSON.stringify(record));
+    await page.close();
+};
+
 (async () => {
-    let browser = await puppeteer.launch({ headless: false });
+    fs.mkdirSync("./temp");
+    browser = await puppeteer.launch({ headless: false });
 
-    const doAdvancedQuery = async record => {
-        const { name, organization: org } = record;
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1200, height: 1500 });
-        await page.goto(google_advanced_search);
+    for (let item of data) {
+        if (fs.existsSync(`./temp/${item.id}`)) {
+            continue;
+        }
 
-        await page.waitForSelector(".otByu > .yIQOUb > .jYcx0e > .PLLUQc > #CwYCWc");
-        await page.type(
-            ".otByu > .yIQOUb > .jYcx0e > .PLLUQc > #CwYCWc",
-            `"${name}" "${org}" "lab"`
-        );
+        await doAdvancedQuery(item).catch(e => {
+            console.log(e);
+        });
+        await timeout(5000);
+    }
 
-        await page.waitForSelector("form > .otByu > .yIQOUb > .jYcx0e > .jfk-button");
-        await page.click("form > .otByu > .yIQOUb > .jYcx0e > .jfk-button");
-
-        await page.waitForNavigation();
-        await page.waitFor(1000);
-
-        let res = await page.$(".g .rc .s");
-        const val = await page.evaluate(el => el.innerText, res);
-        const lab = parseLab(val);
-        record.lab = lab;
-        await page.close();
-    };
-
-    await Promise.all(
-        mockData.map(item => {
-            return doAdvancedQuery(item).catch((e) => { console.log(e); });
-        })
-    );
-
-    console.log("lbao", mockData);
+    await browser.close();
 })();
